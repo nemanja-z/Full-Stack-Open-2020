bd711029fe65c199a8f34f0bd883d3b667910ae5
nemanja-z <nemanja14zivanovic@gmail.com>
2020-04-26 22:24:12 +0200

    favoriteBlog

--- part4/blog/tests/favoriteBlog.test.js
+++ part4/blog/tests/favoriteBlog.test.js
@@ -1,12 +1,19 @@
 const favoriteBlog = require('../utils/list_helper').favoriteBlog
 
 describe('Favorite blog', () => {
-    const oneBlog = {
+    const oneBlog = [{
         title: "React patterns",
         author: "Michael Chan",
         likes: 13
-    }
+    }]
     const blog = [
+        {
+            _id: "5a422aa71b54a676254d17f8",
+            title: "React patterns",
+            author: "Michael Chan",
+            url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+            likes: 13
+        },
         {
             _id: "5a422aa71b54a676234d17f8",
             title: "Go To Statement Considered Harmful",
@@ -31,10 +38,14 @@ describe('Favorite blog', () => {
     test('when list is empty', () => {
         const blog = [];
         const result = favoriteBlog(blog)
-        expect(result).toBe(0)
+        expect(result).toBeNull()
     })
     test('when list have one post', () => {
         const result = favoriteBlog(oneBlog)
+        expect(result).toEqual(oneBlog)
+    })
+    test('when list have more than one post', () => {
+        const result = favoriteBlog(blog)
         expect(result).toEqual(oneBlog)
     })
 })
--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -8,17 +8,34 @@ const total = blogs => {
     return blogs.length === 0 ? 0 : blogs.reduce(reduce, 0)
 }
 const favoriteBlog = blog => {
-    const maxPro = Math.max(...blog.map(b => b.likes))
-    const obj = blog.find(b => b.likes === maxPro)
-    return {
-        title: obj['title'],
-        author: obj['author'],
-        likes: obj['likes']
+    if (blog.length === 0) {
+        return null;
+    } else {
+        const maxPro = Math.max(...blog.map(b => b.likes))
+        const obj = blog.find(b => b.likes === maxPro)
+        return [{
+            title: obj['title'],
+            author: obj['author'],
+            likes: obj['likes']
+        }]
     }
 }
+const mostBlogs = blog => {
+    if (blog.length === 0) {
+        return null;
+    }
+    else {
+        const res = blog.reduce((sum, item) => {
+            sum[item.author] = (sum[item.author] || 0) + 1;
+            return sum, 0
+        })
+    } return res
+}
+
 
 module.exports = {
     dummy,
     total,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs
 }
