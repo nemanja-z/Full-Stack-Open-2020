f054482d37c86680f68bfa59b8dbc96746f75347
nemanja-z <nemanja14zivanovic@gmail.com>
2020-05-28 18:24:57 +0200

    8.12

---
+++ part8/library-frontend/src/components/BirthForm.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import { useQuery, useMutation, gql } from '@apollo/client'
+import { ALL_AUTHORS } from './Authors'
+import Select from 'react-select';
+const SET_BIRTHYEAR = gql`
+    mutation editAuthor($name:String!,$setBornTo:Int!){
+        editAuthor(
+            name:$name,
+            setBornTo:$setBornTo){
+                name
+                born
+            }
+    }`
+const BirthForm = () => {
+    const [name, setName] = useState('')
+    const [born, setBorn] = useState('')
+    const [updateAuthor] = useMutation(SET_BIRTHYEAR, {
+        refetchQueries: [{ query: ALL_AUTHORS }]
+    })
+    const authors = useQuery(ALL_AUTHORS)
+    const editAge = async e => {
+        e.preventDefault()
+        updateAuthor({ variables: { name, setBornTo: +born } })
+        setName('')
+        setBorn('')
+    }
+    const options = authors.data.allAuthors.map(author => {
+        return { value: author.name, label: author.name }
+    })
+    const handleChange = (name) => {
+        setName(name.value)
+    }
+    const { name: n } = name
+    return (
+        <div>
+            <form onSubmit={editAge}>
+                <Select
+                    value={n}
+                    onChange={handleChange}
+                    options={options} />
+                born:<input
+                    value={born}
+                    onChange={({ target }) => setBorn(target.value)} />
+                <button type='submit'>update</button>
+            </form>
+        </div>
+    )
+}
+export default BirthForm
